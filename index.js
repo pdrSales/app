f2c34738238c8df21cd68e0a9e4df83939c088b0
pdrSales <salesdacostapedro@gmail.com>
2024-09-11 18:57:57 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { select,input } = require ('@inquirer/prompts')
+const { select, input, checkbox } = require ('@inquirer/prompts')
 
 let meta= {
   value: "andar de bicicleta",
   checked: false,
 }
 
-let metas = [ meta]
+let metas = [ meta ]
 
 const cadastrarMeta = async () => {
   const meta = await input ({ message: "Digite a meta:"})
@@ -18,6 +18,33 @@ const cadastrarMeta = async () => {
   metas.push(
     { value: meta, checked: false}
   )
+}
+
+const listarMetas = async () => {
+  const respostas = await checkbox({
+    message : "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+    choices: [...metas],
+    instructions: false,
+  })
+
+  if(respostas.lenght == 0) {
+    console.log("Nenhuma meta selecionada!")
+    return
+  }
+
+  metas.forEach((m) => {
+    m.checked = false
+  })
+  
+  respostas.forEach((resposta) => {
+    const meta = metas.find((m) => {
+      return m.value == resposta
+    })
+    
+    meta.checked = true
+  })
+
+  console.log("Meta(s) marcadas como concluída(s)")
 }
 
 const start = async () => {
@@ -49,7 +76,7 @@ const opcao = await select ({
         console.log(metas)
         break
       case "listar":
-        console.log("vamos listar");
+        await listarMetas()
         break
       case "sair":
         console.log("Até a próxima!")
