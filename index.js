57520bda2de63d93197a66f3b23903595fd625b0
pdrSales <salesdacostapedro@gmail.com>
2024-09-12 18:36:52 -0300

    resolve problema marcadas/desmarcadas

--- index.js
+++ index.js
@@ -26,16 +26,16 @@ const listarMetas = async () => {
     choices: [...metas],
     instructions: false,
   })
+  
+  metas.forEach((m) => {
+    m.checked = false
+  })
 
   if(respostas.lenght == 0) {
     console.log("Nenhuma meta selecionada!")
     return
   }
 
-  metas.forEach((m) => {
-    m.checked = false
-  })
-  
   respostas.forEach((resposta) => {
     const meta = metas.find((m) => {
       return m.value == resposta
